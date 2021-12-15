2cac37657833369ab1d2891342a476b6255aea86
Dogukan Yakupoglu <dogukan.yakupoglu@gmail.com>
2021-12-15 17:54:36 +0300

    completed the project

---
+++ src/components/Todolist/TodoList/TodoList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Todo from '../Todo/Todo'
+
+function TodoList({ setTodos, todos, filteredTodos }) {
+    return (
+        <div className="todo-container">
+            <ul className="todo-list">
+            {filteredTodos.map((todo) => (
+                <Todo todo={todo} setTodos={setTodos} todos={todos} key={todo.id} text={todo.text}/>
+            ))}
+            </ul>
+        </div>
+    )
+}
+
+export default TodoList
---
+++ src/components/Todolist/index.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react'
+import Form from "./Form/Form";
+import TodoList from "./TodoList/TodoList";
+
+function TodoApp() {
+    const [inputText, setInputText] = useState("")
+    const [todos, setTodos] = useState([])
+    const [status, setStatus] = useState("all")
+    const [filteredTodos, setFilteredTodos] = useState([])
+    
+    useEffect(() => {
+        getLocalTodos()
+    }, [])
+
+    useEffect(() => {
+        filterHandler()
+        saveLocalTodos()
+    }, [todos, status])
+
+    const filterHandler = () => {
+        switch (status) {
+            case "completed":
+                setFilteredTodos(todos.filter((todo) => todo.completed === true))
+                break
+            case "uncompleted":
+                setFilteredTodos(todos.filter((todo) => todo.completed === false))
+                break
+            default:
+                setFilteredTodos(todos)
+                break
+        }
+    }
+
+    //Save Todos to LocalStorage
+    const saveLocalTodos = () => {
+        localStorage.setItem("todos", JSON.stringify(todos))
+    }
+    const getLocalTodos = () => {
+        if (localStorage.getItem("todos") === null) {
+            localStorage.setItem("todos", JSON.stringify([]))
+        }
+        else {
+            let todoLocal = JSON.parse(localStorage.getItem("todos"))
+            setTodos(todoLocal)
+        }
+    }
+
+    return (
+        <div>
+            <header>
+                <h1>todos</h1>
+            </header>
+            <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} setStatus={setStatus} />
+            <TodoList setTodos={setTodos} todos={todos} filteredTodos={filteredTodos} />
+        </div>
+    )
+}
+
+export default TodoApp
